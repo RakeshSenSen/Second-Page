64fff9ca2f711849e826b5f980fd2c1338412249
RakeshSenSen <99016333+RakeshSenSen@users.noreply.github.com>
2022-03-14 15:43:38 +0530

    Update spread.py

--- spread.py
+++ spread.py
@@ -23,8 +23,8 @@ with  col3:
      st.image(i,use_column_width=None)
 
 idd='1jx3pR8tohqSRc_fPHLbgM8DcaNrAdQNa8tpxosdDezo'
-idd1=st.sidebar.text_input("ENTER THE Sheet ID")
-df=pd.read_csv(f"https://docs.google.com/spreadsheets/d/{idd}/export?format=csv")
+idd1=st.sidebar.text_input("ENTER THE Sheet ID",)
+df=pd.read_csv(f"https://docs.google.com/spreadsheets/d/{idd1}/export?format=csv")
 #st.write('''Hello''')
 #st.dataframe(df)
 
